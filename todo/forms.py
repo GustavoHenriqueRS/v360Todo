e95c4abdcfc451d98153fc2fe1898addaf834026
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2025-02-19 15:42:22 -0300

    feat: adicionando usuario com email e relação, além de data e hora na lista e itens

--- todo/forms.py
+++ todo/forms.py
@@ -1,12 +1,23 @@
 from django import forms
-from .models import TodoList, TodoItem
+from .models import TodoList, TodoItem, User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email']
+    
+    def validate_unique(self):
+        pass
+
+    def clean_email(self):
+        return self.cleaned_data.get('email')
 
 class TodoListForm(forms.ModelForm):
     class Meta:
         model = TodoList
-        fields = ['nome']
+        fields = ['nome', 'dia']
 
 class TodoItemForm(forms.ModelForm):
     class Meta:
         model = TodoItem
-        fields = ['descricao', 'concluido']
+        fields = ['descricao', 'concluido', 'hora']
--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+import datetime
+
+class User(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.email
 
 class TodoList(models.Model):
+    user = models.ForeignKey(User, related_name='todo_lists', on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
+    dia = models.DateField(default=datetime.date.today())
 
     def __str__(self):
         return self.nome
@@ -10,6 +19,7 @@ class TodoItem(models.Model):
     lista = models.ForeignKey(TodoList, related_name='itens', on_delete=models.CASCADE)
     descricao = models.CharField(max_length=200)
     concluido = models.BooleanField(default=False)
+    hora = models.TimeField(default=datetime.time(hour=0, minute=0))
 
     def __str__(self):
         return self.descricao
